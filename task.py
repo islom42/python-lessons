f100846d21f4784e7a41922f2529757c9901a371
islomnumanovuz <islomnumanov.uz@gmail.com>
2022-07-02 22:17:46 +0500

    feat: Add task answers

---
+++ task.py
@@ -0,0 +1,17 @@
+#1  "Nexia", "Tico", 'Damas' ko'rganlar qilar havas
+print('"Nexia", "Tiko", \'Damas\' ko\'rganlar qilar havas')
+
+#2 5 ning 4-darajasini toping
+print("5 ning 4-darajasi", 5**4)
+
+#3 22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+print(22%4)
+
+#4 Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+print("Kvadratning yuzi", 125**2, "kvadratning perimetri", 125*4)
+
+#5  Diametri 12 ga teng bo'lgan doiraning yuzini toping
+print("Diametri 12 ga teng doiraning yuzi", 3.14 * (12 ** 2))
+
+#6 Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping (Pifagor teoremasidan foydalaning)
+print( "Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi", (6 ** 2 + 7 ** 2) ** (1 / 2))
