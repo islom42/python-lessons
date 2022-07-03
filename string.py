1ffa28faffd4649f12bb26e3f711d181b1bba83e
islomnumanovuz <islomnumanov.uz@gmail.com>
2022-07-03 19:35:12 +0500

    feat: Add string examples

---
+++ string.py
@@ -0,0 +1,25 @@
+ism = "Anvar"
+print("Mening ismim " + ism);
+
+ism = "Anvar";
+familya = "Narzullaev";
+ism_sharif = f"{ism} {familya}"
+print(ism_sharif)
+
+print('Hello World!')
+print('Hello \tWorld!')
+print('Hello \nWorld!')
+
+language = "python"
+print(language.upper())
+print(language.lower())
+
+language = "Russian Language"
+print(language.title())
+print(language.capitalize())
+
+meva = "     olma     "
+print("Men " + meva.lstrip() + " yaxshi ko'raman")
+print("Men " + meva.rstrip() + " yaxshi ko'raman")
+print("Men " + meva.strip() + " yaxshi ko'raman")
+print("Men " + meva + " yaxshi ko'raman")
