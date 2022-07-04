55041ca1ebe082ccc3d5c1723a03f05c11fe5590
islomnumanovuz <islomnumanov.uz@gmail.com>
2022-07-04 20:08:31 +0500

    feat: Add task answers

---
+++ task.py
@@ -0,0 +1,18 @@
+# Foydalanuvchi kiritgan sonning kvadrati va kubini konsolga chiqaruvchi dastur
+son = int(input("Please enter a number\n>>>"));
+print(f"Siz kiritgan son kvadrati {son **2}ga teng")
+print(f"Siz kiritgan son kvadrati {(son **2)*son}ga teng")
+
+# Foydalanuvchining yoshini so'rab, uning tug'ilgan yilini hisoblab, konsolga chiqaruvchi dastur
+
+t_yil = int(input("Tug'ilgan yilizni kiriting\n>>>"))
+print(2022-t_yil, "yoshdasiz")
+
+# Foydalanuvchidan ikki son kiritshni so'rab, kiritilgan sonlarning yig'indisi, ayirmasi, ko'paytmasi va bo'linmasini chiqaruvchi dastur
+a = int(input("Birinchi son\n>>>"))
+b = int(input("Ikkinchi son\n>>>"))
+print(a + b)
+print(a - b)
+print(a / b)
+print(a * b)
+
